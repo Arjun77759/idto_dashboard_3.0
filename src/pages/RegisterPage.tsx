a6efde32483a8f1d859884a3f86a64a76cb90629
Nikhil <nikhil@tartanhq.com>
2025-10-25 13:24:43 +0530

    wip

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,15 +1,7 @@
 import { motion } from 'framer-motion'
+import { Building2, MoveRight } from 'lucide-react'
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-
-// Image assets from Figma
-const imgGroup = "http://localhost:3845/assets/562a7c59aabc101ea749df73fab58615d54ff213.svg"
-const imgGroup1 = "http://localhost:3845/assets/9c94f829cc934b720be198d2fecb30f308ca0851.svg"
-const img = "http://localhost:3845/assets/3062d77ed309b143c337b658b4f3afff93579162.svg"
-const img1 = "http://localhost:3845/assets/db47eb2e3e544fb6057c6357a4c374683013f841.svg"
-const imgLine3 = "http://localhost:3845/assets/a3dbf166fda25d0d40076d20397c24ed4d221ae9.svg"
-const imgGoogleIconStreamlineSvgLogos = "http://localhost:3845/assets/c681046178647741241105969e4263957e1fd2e8.svg"
-const imgMicrosoftIconStreamlineSvgLogos = "http://localhost:3845/assets/2a1be36285e66dcfe55d3f54a8f7e71cd3cd02a6.svg"
 
 const RegisterPage = () => {
   const [email, setEmail] = useState('')
@@ -39,12 +31,7 @@ const RegisterPage = () => {
       {/* Logo */}
       <div className="flex gap-2 items-center px-0 py-1.5 relative w-full">
         <div className="h-8 overflow-hidden relative w-14">
-          <div className="absolute inset-[14.44%_69.17%_14.26%_7.78%]">
-            <img alt="" className="block max-w-none size-full" src={imgGroup} />
-          </div>
-          <div className="absolute inset-[28.75%_7.98%_28.51%_38.52%]">
-            <img alt="" className="block max-w-none size-full" src={imgGroup1} />
-          </div>
+          <img alt="" className="block max-w-none size-full" src={'https://idto-sdk-usage-demo-bucket.s3.ap-south-1.amazonaws.com/dashboard_2.0/idto_logo_black.png'} />
         </div>
       </div>
 
@@ -71,8 +58,8 @@ const RegisterPage = () => {
               </label>
               <div className="bg-[#f7f7f8] border border-[#e7e8ea] border-solid flex gap-2 h-12 items-center px-3 py-2 relative rounded-[6px] w-full">
                 <div className="overflow-hidden relative shrink-0 size-5">
-                  <div className="absolute inset-[12.5%_8.33%]">
-                    <img alt="" className="block max-w-none size-full" src={img} />
+                  <div className="absolute inset-[0.5%_8.33%]">
+                    <Building2 className='size-5 text-[#9296A0]' strokeWidth={2} color='#9296A0' />
                   </div>
                 </div>
                 <div className="flex gap-2 grow items-center justify-center min-h-px min-w-px relative">
@@ -87,11 +74,11 @@ const RegisterPage = () => {
               </div>
             </div>
           </div>
-          
+
           <p className="font-normal leading-[1.4] relative text-[12px] text-[#616675] tracking-[-0.12px] w-full">
             No password required. You'll receive a sign-in link
           </p>
-          
+
           <button
             onClick={handleSubmit}
             className="bg-[#e6e8ff] border border-[#e7e8ea] border-solid flex gap-2 items-center justify-center px-8 py-3.5 relative rounded-lg w-full"
@@ -100,11 +87,7 @@ const RegisterPage = () => {
               Continue
             </p>
             <div className="overflow-hidden relative shrink-0 size-4">
-              <div className="absolute inset-[29.17%_16.67%]">
-                <div className="absolute bottom-[-7.96%] left-0 right-[-9.94%] top-[-7.95%]">
-                  <img alt="" className="block max-w-none size-full" src={img1} />
-                </div>
-              </div>
+              <MoveRight className='size-4 text-[#0019ff]' strokeWidth={2} color='#0019ff' />
             </div>
           </button>
         </div>
@@ -112,11 +95,10 @@ const RegisterPage = () => {
         {/* Social Login Section */}
         <div className="flex flex-col gap-4 items-center relative w-full">
           <div className="h-0 relative w-full">
-            <div className="absolute bottom-0 left-0 right-0 top-[-1px]">
-              <img alt="" className="block max-w-none size-full" src={imgLine3} />
+            <div className="absolute bottom-0 left-0 right-0 top-[-1px] border-b border-[#9296A0] border-solid">
             </div>
           </div>
-          
+
           <button
             onClick={handleGoogleSignup}
             className="bg-[#f7f7f8] border border-[#e7e8ea] border-solid flex gap-2 items-center justify-center px-8 py-3.5 relative rounded-lg w-full"
@@ -125,10 +107,22 @@ const RegisterPage = () => {
               Signup with Google
             </p>
             <div className="relative shrink-0 size-4">
-              <img alt="" className="block max-w-none size-full" src={imgGoogleIconStreamlineSvgLogos} />
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+                <g clip-path="url(#clip0_362_2666)">
+                  <path d="M15.6767 8.17422C15.6767 7.53015 15.6245 7.06015 15.5114 6.57275H8.15674V9.47975H12.4737C12.3867 10.2022 11.9167 11.2902 10.8723 12.0212L10.8576 12.1186L13.183 13.92L13.3441 13.9361C14.8237 12.5696 15.6767 10.559 15.6767 8.17422Z" fill="#4285F4" />
+                  <path d="M8.15673 15.8334C10.2717 15.8334 12.0472 15.137 13.3441 13.936L10.8723 12.0211C10.2108 12.4824 9.323 12.8045 8.15673 12.8045C6.08527 12.8045 4.32713 11.438 3.7004 9.54932L3.60855 9.55712L1.19056 11.4284L1.15894 11.5163C2.44707 14.0752 5.093 15.8334 8.15673 15.8334Z" fill="#34A853" />
+                  <path d="M3.7005 9.5495C3.53511 9.0621 3.43941 8.53983 3.43941 8.00023C3.43941 7.46057 3.53513 6.93837 3.6918 6.45097L3.68741 6.34717L1.23912 4.4458L1.15902 4.4839C0.62812 5.54577 0.323486 6.7382 0.323486 8.00023C0.323486 9.26227 0.62812 10.4546 1.15902 11.5165L3.7005 9.5495Z" fill="#FBBC05" />
+                  <path d="M8.15673 3.19537C9.62763 3.19537 10.6198 3.83074 11.1856 4.3617L13.3963 2.20317C12.0386 0.941137 10.2717 0.166504 8.15673 0.166504C5.093 0.166504 2.44707 1.92464 1.15894 4.4835L3.6917 6.45057C4.32713 4.56187 6.08527 3.19537 8.15673 3.19537Z" fill="#EB4335" />
+                </g>
+                <defs>
+                  <clipPath id="clip0_362_2666">
+                    <rect width="16" height="16" fill="white" />
+                  </clipPath>
+                </defs>
+              </svg>
             </div>
           </button>
-          
+
           <div className="flex flex-col gap-4 items-center relative w-full">
             <button
               onClick={handleMicrosoftSignup}
@@ -138,7 +132,19 @@ const RegisterPage = () => {
                 Signup with Microsoft
               </p>
               <div className="relative shrink-0 size-4">
-                <img alt="" className="block max-w-none size-full" src={imgMicrosoftIconStreamlineSvgLogos} />
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+                  <g clip-path="url(#clip0_362_2675)">
+                    <path d="M7.61238 7.61214H0.166748V0.166504H7.61238V7.61214Z" fill="#F1511B" />
+                    <path d="M15.8332 7.61214H8.3877V0.166504H15.8332V7.61214Z" fill="#80CC28" />
+                    <path d="M7.6122 15.8333H0.166748V8.3877H7.6122V15.8333Z" fill="#00ADEF" />
+                    <path d="M15.8332 15.8333H8.3877V8.3877H15.8332V15.8333Z" fill="#FBBC09" />
+                  </g>
+                  <defs>
+                    <clipPath id="clip0_362_2675">
+                      <rect width="16" height="16" fill="white" />
+                    </clipPath>
+                  </defs>
+                </svg>
               </div>
             </button>
           </div>
