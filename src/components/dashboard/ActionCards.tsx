052ae344145f15fa25422f877ce608e22184b3d5
Nikhil <nikhil@tartanhq.com>
2025-11-25 17:57:43 +0530

    first commit

--- src/components/dashboard/ActionCards.tsx
+++ src/components/dashboard/ActionCards.tsx
@@ -95,7 +95,7 @@ const ActionCards = () => {
     ]
 
     const renderButton = (card: ActionCardConfig) => {
-        const isBalance = card.variant === 'balance'
+        const isBalance = card.variant === 'balance1'
         const isPromo = card.variant === 'promo'
 
         if (isPromo) {
